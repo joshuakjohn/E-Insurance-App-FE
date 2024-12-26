05b0095e1159e01d95b6185e5b70a982b1d93193
santhosh <santhosh20sekar@gmail.com>
2024-12-26 11:44:23 +0530

    [santhosh] feat: created the plan template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { PlanComponent } from './components/plan/plan.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
+    PlanComponent,
   ],
   imports: [
     BrowserModule,
