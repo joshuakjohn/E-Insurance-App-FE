71dd832eaff4704c7776769e07f626a4f9917cc8
Joshua K John <joshuakjohn7@gmail.com>
2024-12-28 14:55:17 +0530

    [joshua] feat: added api call and form validation based on role

--- src/app/components/login-and-signup/login-and-signup.component.ts
+++ src/app/components/login-and-signup/login-and-signup.component.ts
@@ -73,9 +73,18 @@ get signinFormControls() { return this.signinForm.controls; }
 
   handleRegistration(){
     if(this.signupForm.valid){
+      let data = {};
+
       const { signup_role, username, email, password, phno, age, region, image } = this.signupForm.value
-      if(signup_role === 'Customer'){
-        this.httpService.postApiCall(`/api/v1/customer/register`, {username, email, password, phno, age, region}).subscribe({
+      if(signup_role === 'Customer')
+        data = {username, email, password, phno, age, region}
+      else if(signup_role === 'Agent')
+        data = {username, email, password, phno, region}
+      else if(signup_role === 'Employee' || signup_role === 'Admin')
+        data = {username, email, password, phno}
+
+      const role_lower = signup_role.toLowerCase()
+        this.httpService.postApiCall(`/api/v1/${role_lower}/register`, data).subscribe({
         next: (res) => {
           console.log(res)
         },
@@ -83,13 +92,11 @@ get signinFormControls() { return this.signinForm.controls; }
           console.log(err)
         }
       })
-      }
     }
   }
 
   handleSignin(){
     if(this.signinForm.valid){
-      console.log(this.signinForm.value)
       const { signin_role, email, password} = this.signinForm.value
       const role_lower = signin_role.toLowerCase()
       this.httpService.postApiCall(`/api/v1/${role_lower}`, {email, password}).subscribe({
@@ -106,6 +113,25 @@ get signinFormControls() { return this.signinForm.controls; }
   onRoleChange(newRole: string) {
     console.log('Role changed to:', newRole);
     this.role = newRole;
+    this.updateConditionalFields()
+  }
+
+  updateConditionalFields() {
+    // Handle `age` field
+    if (this.role === 'Customer') {
+      this.signupForm.get('age')?.setValidators([Validators.required, Validators.pattern('^[0-9]+$')]);
+    } else {
+      this.signupForm.get('age')?.clearValidators();
+    }
+    this.signupForm.get('age')?.updateValueAndValidity();
+
+    // Handle `region` field
+    if (this.role === 'Customer' || this.role === 'Agent') {
+      this.signupForm.get('region')?.setValidators([Validators.required]);
+    } else {
+      this.signupForm.get('region')?.clearValidators();
+    }
+    this.signupForm.get('region')?.updateValueAndValidity();
   }
 
 }
