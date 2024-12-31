48e54a9f1bbb20b7c9425ab6430a25fc2d03a7b6
AnjaliGour0643 <gour26000@gmail.com>
2024-12-31 12:48:43 +0530

    added admin component : ability to create plan and schemes (#11)
    * [anjali] feat : added admin component and basic ui to create a plan
    
    * [anjali] feat : added api call to create a plan without admin authorization
    
    * [anjali] refactored : made the description field as optional while creating a plan
    
    * [anjali] feat : added basic ui to create a scheme
    
    * [anjali] refactor : improved the ui for creating a new plan
    
    * [anjali] refactored: improved UI to create a new scheme
    
    * [anjali] refactored : improved the ui for creating a scheme by manually passing planId
    
    * [anjali] refactor : implemented light theme to admin component
    
    * [anjali] feat : added field to add highlights in a plan while creating any new plan
    
    * [anjali] feat : implemented code to select plan while creating scheme
    
    * [anjali] feat : implemented code to add highlights while creating a plan by admin

---
+++ src/app/components/admin/plan-form/plan-form.component.ts
@@ -0,0 +1,84 @@
+import { HttpHeaders } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpService } from 'src/app/services/http-service/http.service';
+
+@Component({
+  selector: 'app-plan-form',
+  templateUrl: './plan-form.component.html',
+  styleUrls: ['./plan-form.component.scss']
+})
+export class PlanFormComponent {
+  planForm!: FormGroup;
+  isFormSubmitted = false;
+  highlights: string[] = [];
+  
+  header: HttpHeaders;
+  constructor(private formBuilder: FormBuilder, private httpService: HttpService, private router: Router) {
+    const authToken = localStorage.getItem('authToken');
+    this.header = authToken
+      ? new HttpHeaders().set('Authorization', `Bearer ${authToken}`)
+      : new HttpHeaders();
+    if (!authToken) {
+      console.error('Authorization token is missing');
+    }
+  }
+
+  ngOnInit(): void {
+    this.planForm = this.formBuilder.group({
+      planName: ['', Validators.required],
+      description: [''],
+      category: ['', Validators.required]
+    });
+    this.addHighlight();
+  }
+
+  get planFormControls() {
+    return this.planForm.controls;
+  }
+
+  addHighlight(): void {
+    if (this.highlights.length < 5) {
+      const highlightControlName = `highlight${this.highlights.length}`;
+      this.planForm.addControl(highlightControlName, new FormControl(''));
+      this.highlights.push(highlightControlName);
+    }
+  }
+
+  removeHighlight(index: number): void {
+    const highlightControlName = this.highlights[index];
+    this.planForm.removeControl(highlightControlName);
+    this.highlights.splice(index, 1);
+  }
+
+
+  handleSubmit() {
+    this.isFormSubmitted = true;
+    if (this.planForm.valid) {
+      const { planName, description, category } = this.planForm.value;
+      const highlights = this.highlights.map(h => this.planForm.get(h)?.value).filter(h => h);
+
+      const payload: { planName: string; category: string; description?: string, highlight?: string[] } = {planName, category, highlight: highlights};
+    
+      // Add description if it's not empty
+      if (description) {
+        payload.description = description;
+      }
+      console.log('-------')
+      this.httpService.postApiCall('/api/v1/plan', payload, this.header).subscribe({
+        next: (res) => {
+          console.log('Plan created successfully:', res);
+          this.router.navigate(['/dashboard/plans']); 
+        },
+        error: (error) => {
+          console.error('Error creating plan:', error);
+        },
+      });
+    }
+  }
+
+  onClose(): void {
+    this.router.navigate(['/dashboard/plans']); 
+  }
+}
