228d43e994fa8b917ff147d005f77adcf8033621
Anjali Gour <gour26000@gmail.com>
2025-01-02 18:53:24 +0530

    [anjali] feat : added admin login component

---
+++ src/app/components/admin-login/admin-login.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpService } from 'src/app/services/http-services/http.service';
+
+@Component({
+  selector: 'app-admin-login',
+  templateUrl: './admin-login.component.html',
+  styleUrls: ['./admin-login.component.scss']
+})
+export class AdminLoginComponent {
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private httpService: HttpService, private router: Router) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  // Convenience getter for easy access to form fields
+  get loginFormControls() { return this.loginForm.controls; }
+
+  handleLogin() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      console.log('hello')
+
+      this.httpService.postApiCall('/api/v1/admin', { email, password }).subscribe({
+        next: (res) => {
+          console.log(res);
+
+          // Navigate to dashboard
+          this.router.navigate(['/admin/dashboard']);
+        },
+        error: (err) => {
+          console.error(err);
+        }
+      });
+    }
+  }
+}
+
+interface LoginResponse {
+  code: number;
+  token: string;
+  email: string;
+  username: string; 
+  message: string;
+}
